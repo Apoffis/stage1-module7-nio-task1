08abe4639eb58fcc17836122eaf140318b7c55ea
Apoffis <abc22_08@mail.ru>
2022-10-26 21:35:36 +0400

    nio task 1 commit

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,40 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 
 public class FileReader {
 
+    public static void main(String[] args) {
+        File file = new File("src/main/resources/Profile.txt");
+        System.out.println(new FileReader().getDataFromFile(file));
+    }
+
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Map<String, String> profile = new HashMap<>();
+        List<String> strings = new ArrayList<>();
+
+        try (Stream<String> stream = Files.lines(Paths.get(file.getPath()))) {
+            stream.forEach(i -> strings.add(i));
+
+            for (String l : strings) {
+                String[] keyVal = l.split(": ");
+                profile.put(keyVal[0], keyVal[1]);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return new Profile(profile.get("Name"), Integer.parseInt(profile.get("Age")), profile.get("Email"), Long.parseLong(profile.get("Phone")));
     }
 }
